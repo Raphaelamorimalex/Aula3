deaf5d3953762673efb418482b41741c7d190946
Raphael.AmorimAlex <Raphael.amorimalex@gmail.com>
2021-02-22 17:28:00 -0300

    Aula 3 sobre prevendo erros na declaração de variáveis

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var currentAge:Int
+    currentAge = 22
+    println(currentAge)
+    var nome =  "Raphael Alexander Moura Amorim"
+    nome = 2021.toString() + "  Raphael Alexander Moura Amorim"
+    println(nome)
+}
